052d642c5e37785b5f0849cdf57a95d914573600
Anchal Agrawal <agrawalanchal62@gmail.com>
2025-02-28 12:24:57 -0500

    updated Week1

---
+++ Week1/allcaps.py
@@ -0,0 +1,9 @@
+
+
+
+if __name__ == "__main__":
+    animals=['tiger', 'elephant', 'monkey', 'zebra', 'panther']
+    for i in range(len(animals)):
+        print(animals[i].upper())
+       
+
---
+++ Week1/sum.py
@@ -0,0 +1,11 @@
+
+def sum_of_integer(a,b):
+    c = a+b
+    return c
+
+
+if __name__ == "__main__":
+    a = 74
+    b =8
+
+    print(sum_of_integer(a,b))
